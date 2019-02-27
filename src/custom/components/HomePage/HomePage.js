27b4e62722c82cb2f3d0cce0fad9c3b6b88e720f
andremachon <ajbmachon@gmail.com>
2019-02-27 12:10:04 +0100

    Updated My Homepage Component according to documentation.
    Added components context, as well as a Button

--- src/custom/components/HomePage/HomePage.js
+++ src/custom/components/HomePage/HomePage.js
@@ -1,7 +1,8 @@
 // Created by Andre Machon 28/01/2019
 import React from "react";
 import PropTypes from "prop-types";
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
+import { withComponents } from "@reactioncommerce/components-context";
 import Typography from "@material-ui/core/Typography";
 import HomePageFeatured from "custom/components/HomePageFeatured"
 
@@ -11,26 +12,46 @@ const styles = (theme) => ({
     display: "flex",
     justifyContent: "center",
     marginBottom: theme.spacing.unit * 2
+  },
+
+  text: {
+    fontSize: 28
   }
 });
 
+@withComponents
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      isWaiting: false
+    };
 
   }
 
+  onBtnClick = () => {
+    this.setState({ isWaiting: true }); setTimeout(() => { this.setState({ isWaiting: false }); }, 3000);
+  };
+
   render() {
+
+    const { classes, components: { Button } } = this.props;
+
     return (
       <Typography>
 
         <div className="homepage">
           <h1>This is the Homepage!!!</h1>
+          <Button
+            title="Default"
+            className="myBtn"
+            isWaiting={this.state.isWaiting}
+            onClick={this.onBtnClick}
+          >My Own Reaction Button!</Button>
           <HomePageFeatured/>
 
-          <p>This is our placeholder text</p>
+          <p className={classes.text}>This is our placeholder text</p>
         </div>
       </Typography>
     );
@@ -38,7 +59,13 @@ class HomePage extends React.Component {
 }
 
 HomePage.propTypes = {
-  classes: PropTypes.object
+  classes: PropTypes.object,
+  components: PropTypes.shape({
+    /**
+     * Use Reaction Design System Button
+     */
+    Button: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
+  }).isRequired
 };
 
 export default withStyles(styles)(HomePage);
