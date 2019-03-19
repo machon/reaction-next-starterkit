bb4482a760b0eddf8881ad34d03f15707b49c8f3
andremachon <ajbmachon@gmail.com>
2019-03-19 19:39:47 +0100

    Updated renderNavItem function to be more robust
    Now works with multiple classnames instead of checking for exact string

--- src/custom/components/custom-render-functions/NavigationDesktopCustom.js
+++ src/custom/components/custom-render-functions/NavigationDesktopCustom.js
@@ -1,9 +1,18 @@
 // Created by Andre Machon 19/03/2019
 import { NavigationItemDesktop } from "components/NavigationDesktop";
 
+/**
+ * If a filter is passed this function only renders
+ * NavigationItemDesktop Components if they have a class matching the filter
+ * @param navItem
+ * @param index
+ * @param filter
+ * @returns {NavigationItemDesktop} React Navigation Component
+ */
 function renderNavItem(navItem, index, filter = null) {
   if (filter) {
-    if (navItem.navigationItem.data.classNames === filter) return <NavigationItemDesktop key={index}
+    // console.log();
+    if (navItem.navigationItem.data.classNames.split(' ').includes(filter)) return <NavigationItemDesktop key={index}
                                                                                          navItem={navItem}/>;
   } else {
     return <NavigationItemDesktop key={index} navItem={navItem}/>;
@@ -11,6 +20,11 @@ function renderNavItem(navItem, index, filter = null) {
 }
 
 export default {
+  /**
+   * render function for desktop navigation items, only renders items filtered via classname
+   * @param props
+   * @returns {*}
+   */
   render(props) {
     const { navItems, filter } = props;
 
