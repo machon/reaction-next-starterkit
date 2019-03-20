c253cd200d164bcb0b021f90490cc2b50a4b8fe3
andremachon <ajbmachon@gmail.com>
2019-03-20 22:57:08 +0100

    Updated FeaturedCategoryStepper to work with tags
    Now tag images and links get displayed, but only for tags with the
    metafield keyword "featuredCategory"

--- src/custom/components/FeaturedCategoryStepper/FeaturedCategoryStepper.js
+++ src/custom/components/FeaturedCategoryStepper/FeaturedCategoryStepper.js
@@ -15,34 +15,6 @@ import { autoPlay } from 'react-swipeable-views-utils';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const tutorialSteps = [
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-  },
-  {
-    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-];
-
 const styles = theme => ({
   root: {
     maxWidth: "100%",
@@ -123,8 +95,9 @@ class FeaturedCategoryStepper extends React.Component {
   render() {
     const { classes, theme } = this.props;
     const { activeStep } = this.state;
-    const maxSteps = tutorialSteps.length;
-
+    const featuredTags = this.getFeaturedCategoryTags();
+    const maxSteps = featuredTags.length;
+    
     return (
       <div className={classes.root}>
         <Paper square elevation={0} className={classes.header}>
@@ -137,37 +110,38 @@ class FeaturedCategoryStepper extends React.Component {
           enableMouseEvents
           interval={5000}
         >
-          {tutorialSteps.map((step, index) => {
+          {featuredTags.map((tag, index) => {
 
-            let nextStep = tutorialSteps[index + 1];
-            let nextStep2 = tutorialSteps[index + 2];
-            if (tutorialSteps.length <= index + 2) {
-              nextStep2 = tutorialSteps[0];
+            let nextTag = featuredTags[index + 1];
+            let nextTag2 = featuredTags[index + 2];
+            if (featuredTags.length <= index + 2) {
+              nextTag2 = featuredTags[0];
             }
-            if (tutorialSteps.length <= index + 1) {
-              nextStep = tutorialSteps[0];
-              nextStep2 = tutorialSteps[1];
+            if (featuredTags.length <= index + 1) {
+              nextTag = featuredTags[0];
+              nextTag2 = featuredTags[1];
 
             }
 
             return (
-              <div key={step.label}>
+              
+              <div key={tag.name}>
                 {Math.abs(activeStep - index) <= 2 ? (
                   <Fragment>
                     <div style={{ display: "inline", position: "relative" }}>
-                      <img className={classes.img} src={step.imgPath} alt={step.label}/>
-                      <a style={{ position: "absolute", bottom: "5px", left: "30%" }} href={step.imgPath}>
-                        <span className={classes.catCaption}>Featured Category 1</span></a>
+                      <img className={classes.img} src={tag.heroMediaUrl} alt={tag.name}/>
+                      <a style={{ position: "absolute", bottom: "5px", left: "30%" }} href={`/tag/${tag.slug}`}>
+                        <span className={classes.catCaption}>{tag.name}</span></a>
                     </div>
                     <div style={{ display: "inline", position: "relative" }}>
-                      <img className={classes.img} src={nextStep.imgPath} alt={nextStep.label}/>
-                      <a style={{ position: "absolute", bottom: "5px", left: "30%" }} href={nextStep.imgPath}>
-                        <span className={classes.catCaption}>Featured Category 2</span></a>
+                      <img className={classes.img} src={nextTag.heroMediaUrl} alt={nextTag.name}/>
+                      <a style={{ position: "absolute", bottom: "5px", left: "30%" }} href={`/tag/${nextTag.slug}`}>
+                        <span className={classes.catCaption}>{nextTag.name}</span></a>
                     </div>
                     <div style={{ display: "inline", position: "relative" }}>
-                      <img className={classes.img} src={nextStep2.imgPath} alt={nextStep2.label}/>
-                      <a style={{ position: "absolute", bottom: "5px", left: "30%" }} href={nextStep2.imgPath}>
-                        <span className={classes.catCaption}>Featured Category 3</span></a>
+                      <img className={classes.img} src={nextTag2.heroMediaUrl} alt={nextTag2.name}/>
+                      <a style={{ position: "absolute", bottom: "5px", left: "30%" }} href={`/tag/${nextTag2.slug}`}>
+                        <span className={classes.catCaption}>{nextTag2.name}</span></a>
                     </div>
                   </Fragment>
                 ) : null}
